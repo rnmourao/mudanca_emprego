e0e6912f0cfd0f8cd8cb20c4db2f86a306aa2fb3
rnmourao <robertonunesmourao@yahoo.com.br>
2018-06-10 17:50:06 -0300

    novos itens

--- src/04_tabelas_novo.py
+++ src/04_tabelas_novo.py
@@ -7,21 +7,37 @@ import string
 import unicodedata
 
 def efetuar_parse(texto):
+    'Tirar todos os prefixos de tags.'
+
     from StringIO import StringIO
     import xml.etree.ElementTree as ET
 
     it = ET.iterparse(StringIO(texto))
     for _, el in it:
         if '}' in el.tag:
-            el.tag = el.tag.split('}', 1)[1]  # tirar todos prefixo em chaves {}
+            el.tag = el.tag.split('}', 1)[1].replace('sov:', '')
     return it.root
 
 
 def obtem_campos(elemento, chave=''):
-    chave += '_' + str(elemento.tag)
+    'Extrair recursivamente conteudo de um trecho de XML.'
+
+    if chave == '':
+        chave = elemento.tag
+    else:
+        chave += '_' + elemento.tag
     filhos = list(elemento)
     if not filhos:
-        linha[chave] = elemento.text
+        if elemento.text:
+            linha[chave] = elemento.text
+
+        if 'name' in elemento.attrib:
+            chave += '_' + elemento.attrib['name'].replace(' ', '')
+            linha[chave] = '1'
+        
+        if 'totalmonths' in elemento.attrib:
+            chave += '_totalmonths'
+            linha[chave] = elemento.attrib['totalmonths']
     else:
         for filho in filhos:
             obtem_campos(filho, chave)
@@ -31,15 +47,20 @@ caminho = '/home/mourao/mudanca_emprego/data/novo/'
 caminho_xml = caminho + 'xml/'
 caminho_csv = caminho + 'csv/'
 
-tabelas = ['contactmethod', 'schoolorinstitution', 'employerorg', 'language', 'licenseorcertification','qualifications', 'reference', 'culture', 'experiencesummary', 'personalinformation', 'traininghistory']
+trechos = {'structuredxmlresume':  ['contactinfo', 'schoolorinstitution', 'employerorg', 'language', 
+                                    'licenseorcertification','qualifications', 'reference'], 
+           'userarea' : ['culture', 'experiencesummary', 'personalinformation', 'traininghistory'] }
 
 # cria tabelas
-for tabela in tabelas:
+for tabela in trechos['structuredxmlresume'] + trechos['userarea']:
     exec(tabela + ' = []')
 
 # ler arquivos xml
 arquivos = [a for a in listdir(caminho_xml) if isfile(join(caminho_xml, a)) and '.xml' in a]
+i = 0
 for arquivo in arquivos:
+    i += 1
+    print  100*i/float(len(arquivos)), arquivo
     with open(join(caminho_xml, arquivo), 'r') as r:
         texto = r.read()
     
@@ -54,20 +75,21 @@ for arquivo in arquivos:
     # executar parse
     raiz = efetuar_parse(texto)
 
-    curriculo = raiz.find('structuredxmlresume')
+    for trecho in ['structuredxmlresume', 'userarea']:
+        curriculo = raiz.find(trecho)
 
-    # povoar tabelas
-    for tabela in tabelas:
-        for item in curriculo.iterfind('.//' + tabela):
-            linha =  { 'id' : arquivo }
-            obtem_campos(item)
-            exec(tabela + '.append(linha)')
+        # povoar tabelas
+        tabelas = trechos[trecho]
+        for tabela in tabelas:
+            for item in curriculo.iterfind('.//' + tabela):
+                linha =  { 'id' : arquivo }
+                obtem_campos(item)
+                exec(tabela + '.append(linha)')
 
-for tabela in tabelas:
+for tabela in trechos['structuredxmlresume'] + trechos['userarea']:
     tab = None
     exec('tab = ' + tabela)
     pd.DataFrame(tab).to_csv(caminho_csv + tabela + '.csv', encoding='utf-8', index=False, sep=',')
 
-# IdValue
 # RevisionDate
 # ResumeQuality
